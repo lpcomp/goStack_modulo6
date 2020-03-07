98f5c12231bff4172f794312286d98819c387d77
Luiz Paulo Baçal de Vasconcelos <luizv993@gmail.com>
2020-03-07 15:08:33 -0300

    Desafio 06
    Parte 1, 2 e 3 feitas do desafio

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/state-in-constructor */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
@@ -32,11 +33,16 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    pagina: 1,
+    refreshing: false,
+    loading: false,
   };
 
   async componentDidMount() {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
+
+    this.setState({ loading: true });
 
     // console.tron.log(user);
 
@@ -44,12 +50,46 @@ export default class User extends Component {
 
     // console.tron.log(response);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
+
+  refreshList = () => {
+    console.log('dando refresh');
+    this.setState({ refreshing: true, stars: [] }, () =>
+      this.loadMore('refresh')
+    );
+  };
+
+  loadMore = async tipo => {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+    let { pagina } = this.state;
+    const { stars } = this.state;
+
+    pagina += 1;
+    console.log(tipo);
+    if (tipo === 'refresh') pagina = 1;
+
+    const response = await api.get(
+      `/users/${user.login}/starred?page=${pagina}`
+    );
+
+    if (response.data.length <= 0) return;
+    // stars.push(response.data);
+    console.log(stars);
+    console.log(response.data);
+
+    this.setState({
+      stars: [...stars, ...response.data],
+      pagina,
+      refreshing: false,
+    });
+    console.log(this.state.stars);
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
+    const { stars, loading, refreshing } = this.state;
 
     const user = navigation.getParam('user');
 
@@ -61,19 +101,27 @@ export default class User extends Component {
           <Bio>{user.bio}</Bio>
         </Header>
 
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading ? (
+          <ActivityIndicator color="#7159c1" style={{ marginTop: '5%' }} />
+        ) : (
+          <Stars
+            data={stars}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
